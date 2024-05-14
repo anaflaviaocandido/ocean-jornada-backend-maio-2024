c8b12fa32b5a5fb9e62a9d2ab559995263c3820d
Ana Flávia <140007311+anaflaviaocandido@users.noreply.github.com>
2024-05-14 14:35:15 +0000

    [GET] /oi

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-
-app.get('oi/', function (req, res) {
-  res.send('Olá Mundo')
+app.get('/', function (req, res) {
+    res.send("Hello World")
 })
 
+app.get('/oi', function (req, res) {
+    res.send("Olá, mundo!")
+})
 app.listen(3000)
