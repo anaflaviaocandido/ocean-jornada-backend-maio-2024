5b61a239a9ce727f40e48d178bbd7b49fbabcbc1
Ana Flávia <140007311+anaflaviaocandido@users.noreply.github.com>
2024-05-14 17:11:05 +0000

    Read All [GET]/item

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ app.get('/', function (req, res) {
     res.send("Hello World")
 })
 
+//Rota [GET]
 app.get('/oi', function (req, res) {
     res.send("Olá, mundo!")
 })
+
+//Lista de itens
+const itens = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+//Endpoint de Read All [GET] /item
+app.get('/item', function (req, res) {
+    res.send(itens)
+})
+
 app.listen(3000)
